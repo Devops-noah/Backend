98a33d3695cfbd3d9abe4ed176870f3c623ae2e3
Nabintou-FOFANA <nabintou.fofanafn@gmail.com>
2024-11-05 16:51:27 +0100

    Add getAllAnnonces, getAnnonceById

--- app/src/main/java/fr/parisnanterre/noah/Controller/AnnonceController.java
+++ app/src/main/java/fr/parisnanterre/noah/Controller/AnnonceController.java
@@ -1,4 +1,37 @@
-package main.java.fr.parisnanterre.noah.Controller;
+// Annonce.controller
+package fr.parisnanterre.noah.Controller;
 
+import fr.parisnanterre.noah.DTO.Filtre;
+import fr.parisnanterre.noah.Entity.Annonce;
+import fr.parisnanterre.noah.Entity.Pays;
+import fr.parisnanterre.noah.Service.AnnonceServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/annonces")
+//@CrossOrigin(origins = "http://localhost:4200") // Enable CORS for Angular frontend
 public class AnnonceController {
+    @Autowired
+    private AnnonceServiceImpl annonceServiceImpl;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping
+    public List<Annonce> getAllAnnonces() {
+        return annonceServiceImpl.getAllAnnonces();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Annonce> getAnnonceById(@PathVariable Integer id) {
+        return annonceServiceImpl.getAnnonceById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
+
