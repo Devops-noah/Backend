df67ef5e0f9e553a610c16eb21b412419062af64
Nabintou-FOFANA <nabintou.fofanafn@gmail.com>
2024-11-05 16:06:14 +0100

    Modify PaysRepository

--- app/src/main/java/fr/parisnanterre/noah/Repository/PaysRepository.java
+++ app/src/main/java/fr/parisnanterre/noah/Repository/PaysRepository.java
@@ -1,4 +1,13 @@
+// Pays repository
 package fr.parisnanterre.noah.Repository;
 
-public interface PaysRepository {
+import fr.parisnanterre.noah.Entity.Pays;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaysRepository extends JpaRepository<Pays, Integer> {
+    // Pays findByCodeISO(String codeISO); // Find country by ISO code
+    Optional<Pays> findByNom(String nom);
 }
+
