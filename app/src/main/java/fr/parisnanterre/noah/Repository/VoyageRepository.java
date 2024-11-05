62833e95d991c10ef6fdbd506c729f1eca7c41db
DIALLO-Hawa <hawadiallo29a@gmail.com>
2024-11-05 16:02:45 +0100

    add interface voyage on repository

--- app/src/main/java/fr/parisnanterre/noah/Repository/VoyageRepository.java
+++ app/src/main/java/fr/parisnanterre/noah/Repository/VoyageRepository.java
@@ -1,4 +1,15 @@
+// Voyage repository
 package fr.parisnanterre.noah.Repository;
 
-public interface VoyageRepository {
+import fr.parisnanterre.noah.Entity.Pays;
+import fr.parisnanterre.noah.Entity.Utilisateur;
+import fr.parisnanterre.noah.Entity.Voyage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VoyageRepository extends JpaRepository<Voyage, Integer> {
+    List<Voyage> findByVoyageur(Utilisateur voyageur); // Find voyages by user (voyageur)
+    List<Voyage> findByDestination(Pays destination); // Find voyages by destination country
 }
+
