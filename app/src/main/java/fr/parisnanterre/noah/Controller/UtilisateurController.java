7e2b71803a15e45a1be2527d32ed60f8296b53d4
amare16 <amarenighatu16@gmail.com>
2024-11-05 17:21:18 +0100

    Add Utilisateur controller

--- app/src/main/java/fr/parisnanterre/noah/Controller/UtilisateurController.java
+++ app/src/main/java/fr/parisnanterre/noah/Controller/UtilisateurController.java
@@ -1,4 +1,51 @@
-package main.java.fr.parisnanterre.noah.Controller;
+package fr.parisnanterre.noah.Controller;
 
+import fr.parisnanterre.noah.Entity.Utilisateur;
+import fr.parisnanterre.noah.Service.UtilisateurServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/utilisateurs")
 public class UtilisateurController {
+    @Autowired
+    private UtilisateurServiceImpl utilisateurService;
+
+    @GetMapping
+    public List<Utilisateur> getAllUtilisateurs() {
+        return utilisateurService.getAllUtilisateurs();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Utilisateur> getUtilisateurById(@PathVariable Integer id) {
+        return utilisateurService.getUtilisateurById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Utilisateur createUtilisateur(@RequestBody Utilisateur utilisateur) {
+        return utilisateurService.createUtilisateur(utilisateur);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Utilisateur> updateUtilisateur(@PathVariable Integer id, @RequestBody Utilisateur utilisateur) {
+        return ResponseEntity.ok(utilisateurService.updateUtilisateur(id, utilisateur));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUtilisateur(@PathVariable Integer id) {
+        utilisateurService.deleteUtilisateur(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<Utilisateur> getUtilisateurByEmail(@RequestParam String email) {
+        return utilisateurService.getUtilisateurByEmail(email)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
