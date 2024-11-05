08bec5398e71d4c7bcb2c384a2cae4a51ea65932
DIALLO-Hawa <hawadiallo29a@gmail.com>
2024-11-05 15:10:23 +0100

    modify Entity Voyage

--- app/src/main/java/fr/parisnanterre/noah/Entity/Voyage.java
+++ app/src/main/java/fr/parisnanterre/noah/Entity/Voyage.java
@@ -1,4 +1,44 @@
+// Voyage entity
 package fr.parisnanterre.noah.Entity;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Voyage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateDepart;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateArrivee;
+
+    private Double poidsDisponible;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "destination_id")
+    private Pays destination;
+
+    @ManyToOne
+    @JoinColumn(name="voyageur_id")
+    private Utilisateur voyageur;
+
+    // Business Logic
+    public boolean creerVoyage(Date dateDepart, Date dateArrivee, Double poidsDisponible, Pays destination, Utilisateur voyageur) {
+        this.dateDepart = dateDepart;
+        this.dateArrivee = dateArrivee;
+        this.poidsDisponible = poidsDisponible;
+        this.destination = destination;
+        this.voyageur = voyageur;
+        return true;
+    }
 }
