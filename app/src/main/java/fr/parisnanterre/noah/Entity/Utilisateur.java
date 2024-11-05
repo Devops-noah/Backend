a82a74945f594fa240f10c30732a1da3e15bcaf8
Oumar <omahamat95@gmail.com>
2024-11-05 15:12:24 +0100

    odify entity Utilisateur

--- app/src/main/java/fr/parisnanterre/noah/Entity/Utilisateur.java
+++ app/src/main/java/fr/parisnanterre/noah/Entity/Utilisateur.java
@@ -1,4 +1,51 @@
+// Utilisateur entity
 package fr.parisnanterre.noah.Entity;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse;
+    private String telephone;
+    private String adresse;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // Relationships
+    @OneToMany(mappedBy = "expediteur")
+    private List<Annonce> annoncesExpediteur;
+
+    @OneToMany(mappedBy = "voyageur")
+    private List<Annonce> annoncesVoyageur;
+
+    @OneToMany(mappedBy = "voyageur")
+    private List<Voyage> voyages;
+
+    // Business logic
+    public boolean authentifier(String password) {
+        return motDePasse.equals(password);
+    }
+
+    public void updateProfile(String nom, String prenom, String telephone, String adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+        this.adresse = adresse;
+    }
 }
+
