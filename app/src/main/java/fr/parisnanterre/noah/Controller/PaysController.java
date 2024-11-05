39ca114d2fd22b422919a3db914449fdf029bed1
Nabintou-FOFANA <nabintou.fofanafn@gmail.com>
2024-11-05 17:16:08 +0100

    Add methods getAllPays, getPaysById, createPays

--- app/src/main/java/fr/parisnanterre/noah/Controller/PaysController.java
+++ app/src/main/java/fr/parisnanterre/noah/Controller/PaysController.java
@@ -1,4 +1,36 @@
-package main.java.fr.parisnanterre.noah.Controller;
+// Pays.controller
+package fr.parisnanterre.noah.Controller;
 
+import fr.parisnanterre.noah.Entity.Pays;
+import fr.parisnanterre.noah.Service.PaysServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/pays")
 public class PaysController {
+    @Autowired
+    private PaysServiceImpl paysServiceImpl;
+
+    @GetMapping
+    public List<Pays> getAllPays() {
+        return paysServiceImpl.getAllPays();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pays> getPaysById(@PathVariable Integer id) {
+        return paysServiceImpl.getPaysById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Pays createPays(@RequestBody Pays pays) {
+        return paysServiceImpl.createPays(pays);
+    }
+
 }
