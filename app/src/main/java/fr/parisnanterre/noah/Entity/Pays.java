05e3d6df37a67c372a5289c2b70495e67b4c114a
Nabintou-FOFANA <nabintou.fofanafn@gmail.com>
2024-11-05 15:03:31 +0100

    Modify Pays Entity

--- app/src/main/java/fr/parisnanterre/noah/Entity/Pays.java
+++ app/src/main/java/fr/parisnanterre/noah/Entity/Pays.java
@@ -1,4 +1,24 @@
+// Pays entity
 package fr.parisnanterre.noah.Entity;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pays {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    private String codeISO;
+
+    private String ville;
 }
+
+
