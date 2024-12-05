c2c786ab201a12bb3927c4aaad9c1c79845ea8d1
amare16 <amarenighatu16@gmail.com>
2024-12-05 10:44:04 +0100

    add seed files - tables

---
+++ app/src/main/java/fr/parisnanterre/noah/seeds/pays.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.pays (id, codeiso, nom, ville) VALUES (1, 'FR', 'FRANCE', 'Paris');
+INSERT INTO public.pays (id, codeiso, nom, ville) VALUES (2, 'DE', 'GERMANY', 'Berlin');
+INSERT INTO public.pays (id, codeiso, nom, ville) VALUES (3, 'GH', 'GHANA', 'Accra');
+INSERT INTO public.pays (id, codeiso, nom, ville) VALUES (6, 'SW', 'SWEDEN', 'Stockholm');
