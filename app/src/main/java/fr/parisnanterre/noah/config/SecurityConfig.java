b0cdbb85a5e3e1714e75f1c6c3bbae8140bab48e
amare16 <amarenighatu16@gmail.com>
2024-12-21 22:43:54 +0100

    add route for annonce detail in security config

--- app/src/main/java/fr/parisnanterre/noah/config/SecurityConfig.java
+++ app/src/main/java/fr/parisnanterre/noah/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 }))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/annonces").permitAll() // Allow public access to `getAllAnnonces`
+                        .requestMatchers("/api/annonces/**").authenticated()
                         .requestMatchers("/api/annonces/filter").permitAll()
                         .requestMatchers("/api/auth/register").permitAll()
                         .requestMatchers("/api/auth/login").permitAll()
